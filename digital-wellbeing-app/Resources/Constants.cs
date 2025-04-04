8a8119b33324b2acb7ec9229ed86daabb962972a
Swaraj <33777798+swarajdhondge@users.noreply.github.com>
2025-04-03 21:29:43 -0400

    Added screen time automation and GitHub Actions release

---
+++ digital-wellbeing-app/Resources/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace digital_wellbeing_app.Resources
+{
+    public static class Constants
+    {
+        public const int IdleThresholdSeconds = 5; // 5 seconds
+    }
+}
---
+++ digital-wellbeing-app/ViewModels/AppUsageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digital_wellbeing_app.ViewModels
+{
+    internal class AppUsageViewModel
+    {
+    }
+}
